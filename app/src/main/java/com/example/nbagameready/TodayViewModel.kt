0f5af2be022a53377811c7c8bbb1e8ef7e56e5af
Kieran <kieranwindley@gmail.com>
2022-03-09 13:22:12 -0500

    working on adding viewmodel functionality

--- app/src/main/java/com/example/nbagameready/TodayViewModel.kt
+++ app/src/main/java/com/example/nbagameready/TodayViewModel.kt
@@ -1,14 +1,36 @@
 package com.example.nbagameready
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.*
 import com.example.nbagameready.network.Game
+import com.example.nbagameready.network.Games
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.crypto.SecretKey
+import kotlin.coroutines.coroutineContext
 
-class TodayViewModel : ViewModel() {
-    val games: MutableLiveData<Game> by lazy {
-        MutableLiveData<Game>()
+class TodayViewModel(application: Application) : AndroidViewModel(application) {
+    private val _apiResponse = MutableLiveData<Games>()
+    val apiResponse: LiveData<Games> = _apiResponse
 
+    val ai: ApplicationInfo = application.packageManager
+        ?.getApplicationInfo(requireContext().packageName, PackageManager.GET_META_DATA)!!
+    val value = ai.metaData["keyValue"]
+    val key = value.toString()
+
+//    init {
+//        getSunData()
+//    }
+
+    fun getGames(currentDateForApi: String, secretKey: SecretKey) {
+        viewModelScope.launch {
+            _apiResponse.value = NbaApi.retrofitService.getGames(currentDateForApi, key)
+        }
     }
+}
 
 
 
