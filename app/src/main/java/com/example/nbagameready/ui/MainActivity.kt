d45c6f74604b8fca9ecd05779a7ab407fb91e12f
Cfrix <cfriloux3@gmail.com>
2022-03-19 17:53:01 -0500

    bottom NavigationView activity_main.xml and MainActivity.kt

--- app/src/main/java/com/example/nbagameready/ui/MainActivity.kt
+++ app/src/main/java/com/example/nbagameready/ui/MainActivity.kt
@@ -12,7 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController: NavController = navHostFragment.navController
+        val navController = navHostFragment.navController
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavView.setupWithNavController(navController)
+
+
+
     }
 
 }
